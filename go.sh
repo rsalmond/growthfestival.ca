1b5fe5e70f5b416b0f0f7720d1cffdf872b72688
rob salmond <rob@salmond.ca>
2025-08-18 16:57:30 -0400

    monster update

--- go.sh
+++ go.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-quarto render
-git checkout docs/CNAME
+quarto render --log-level warning
+tar --no-xattrs -czf growth.tgz -C docs/ .
+scp growth.tgz brazen:~/
+ssh brazen tar xf growth.tgz -C /mnt/kubedata/growth/
